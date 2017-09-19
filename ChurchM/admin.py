41cf07194cf427efde70f06a9aa2b428865b88f5
[Olumide] <[olutaiwo.oni@gmail.com]>
2017-09-19 09:39:51 -0400

    reverting

--- ChurchM/admin.py
+++ ChurchM/admin.py
@@ -2,19 +2,18 @@ from django.contrib import admin
 from ChurchM.models import Ministry, Role, Household, BibleReading, Event, EventType, People, DonationType, Fund, BankDetails, AccountDetails, Attendance
 # Register your models here.
 
-admin.site.register(Ministry, Role, Household, BibleReading,DonationType, Fund, BankDetails, AccountDetails, People,
-                    Event, EventType, Attendance)
-# admin.site.register(Role)
-# admin.site.register(Household)
-# admin.site.register(BibleReading)
-# admin.site.register(DonationType)
-# admin.site.register(Fund)
-# admin.site.register(BankDetails)
-# admin.site.register(AccountDetails)
-# admin.site.register(People)
-# admin.site.register(Event)
-# admin.site.register(EventType)
-# admin.site.register(Attendance)
+admin.site.register(Ministry)
+admin.site.register(Role)
+admin.site.register(Household)
+admin.site.register(BibleReading)
+admin.site.register(DonationType)
+admin.site.register(Fund)
+admin.site.register(BankDetails)
+admin.site.register(AccountDetails)
+admin.site.register(People)
+admin.site.register(Event)
+admin.site.register(EventType)
+admin.site.register(Attendance)
 
 
 
